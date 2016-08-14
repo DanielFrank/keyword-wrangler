df6e9325cc4038ed7030733c7e7e370ec080a64f
Daniel Frank <daniel.frank@lin-digital.com>
2016-08-14 14:59:43 -0700

    First passing test against server

---
+++ spec/e2e/apiSpec.js
@@ -0,0 +1,16 @@
+var request = require('request');
+
+describe('The API', function() {
+  it('should respond to a GET request at /api/keywords/', function(done) {
+    request.get(
+      {
+        'url': 'http://127.0.0.1:4444/api/keywords/',
+        'json': true
+      },
+      function (err, res, body) {
+        expect(res.statusCode).toBe(200);
+        expect(body.foo).toEqual('bar');
+        done();
+      });
+  });
+});
---
+++ src/backend/index.js
@@ -0,0 +1,17 @@
+var Percolator = require('percolator').Percolator;
+
+var port = 4444;
+var server = Percolator({'port': port});
+
+server.route('/api/keywords',
+  {
+    GET: function (req,res){
+      res.object({'foo': 'bar'}).send();
+    }
+  }
+
+);
+
+server.listen(function() {
+  console.log("Server started and listening on port", port);
+});
