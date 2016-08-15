d859b55f1e9bc430cab307a9d1753e4467f2e732
Daniel Frank <daniel.frank@lin-digital.com>
2016-08-14 17:50:54 -0700

    Add frontend (Need to mod backend before can test)

--- src/backend/server.js
+++ src/backend/server.js
@@ -3,7 +3,8 @@ var dbSession = require('../../src/backend/dbSession.js');
 
 
 var Server = function(port){
-  var server = Percolator({'port': port, 'autoLink': false});
+  var server = Percolator({'port': port, 'autoLink': false,
+    'staticDir': __dirname + '/../frontend'});
 
   server.route('/api/keywords',
     {
