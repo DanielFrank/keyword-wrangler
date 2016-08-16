5cc73ea6b91a8716aab5abf259ba2914185d7a7f
Daniel Frank <daniel.frank@lin-digital.com>
2016-08-15 19:39:49 -0700

    Set up to handle different environments (also correct database.json)

--- spec/resetDatabase.js
+++ spec/resetDatabase.js
@@ -1,29 +1,57 @@
 var async = require('async');
+var env = require('../src/backend/env');
+var dbOptions = require('../database.json')[env];
 
 var resetDatabase = function (dbSession, callback) {
-  async.series(
-    [
-      function(callback) {
-        dbSession.remove('keyword', '1', function(err){
-          callback(err);
-        });
-      },
-      function(callback) {
-        dbSession.remove('category', '1', function(err){
-          callback(err);
-        });
-      },
-      function(callback) {
-        dbSession.remove('sqlite_sequence', '1', function(err){
-          callback(err);
-        });
-      }
-    ],
+  if (dbOptions.driver === 'sqlite3') {
 
-    function (err, results) {
-      callback(err);
-    }
-  );
+    async.series(
+      [
+        function(callback) {
+          dbSession.remove('keyword', '1', function(err){
+            callback(err);
+          });
+        },
+        function(callback) {
+          dbSession.remove('category', '1', function(err){
+            callback(err);
+          });
+        },
+        function(callback) {
+          dbSession.remove('sqlite_sequence', '1', function(err){
+            callback(err);
+          });
+        }
+      ],
+
+      function (err, results) {
+        callback(err);
+      }
+    );
+  }
+
+  if (dbOptions.driver === 'mysql') {
+
+    async.series(
+      [
+        function(callback) {
+          dbSession.remove(' TRUNCATE keyword', '1', function(err){
+            callback(err);
+          });
+        },
+        function(callback) {
+          dbSession.remove('TRUNCATE category', '1', function(err){
+            callback(err);
+          });
+        },
+      ],
+
+      function (err, results) {
+        callback(err);
+      }
+    );
+  }
+
 };
 
 module.exports = resetDatabase;
