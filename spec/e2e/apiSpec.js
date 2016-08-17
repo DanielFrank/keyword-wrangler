24350541658c3ba858a2f58dce2f53ac2bc4862e
Daniel Frank <daniel.frank@lin-digital.com>
2016-08-16 21:39:56 -0700

    Add routes for inserting, updating and deleting keywords

--- spec/e2e/apiSpec.js
+++ spec/e2e/apiSpec.js
@@ -81,6 +81,7 @@ describe('The API', function() {
     );
   });
 
+
   it('should respond to a GET request at /api/keywords/categories', function(done) {
     var expected = {
       "_items": [
@@ -124,4 +125,175 @@ describe('The API', function() {
       }
     );
   });
+
+  it('should create a new keyword when receiving a post at /api/keywords/', function(done) {
+    var expected = {
+      "_items": [
+        {'id': 1, 'value': 'Aubergine', 'categoryID': 1},
+        {'id': 2, 'value': 'Onion', 'categoryID': 1}
+      ]
+    };
+    var body = {'value': 'Onion', 'categoryID': 1};
+
+    async.series(
+      [
+        function(callback) {
+          dbSession.insert('category',
+            {'name': 'Vegetable'},
+            function(err){
+              callback(err);
+            });
+        },
+
+        function(callback) {
+          dbSession.insert('keyword',
+            {'value': 'Aubergine', 'categoryID': 1},
+            function(err){
+              callback(err);
+            });
+        },
+      ],
+      function(err, results) {
+        request.post(
+          {
+            'url': 'http://127.0.0.1:4445/api/keywords/',
+            'body': body,
+            'json': true
+          },
+          function (err, res, body) {
+            if (err) throw(err);
+            expect(res.statusCode).toBe(200);
+            request.get(
+              {
+                'url': 'http://127.0.0.1:4445/api/keywords/',
+                'json': true
+              },
+              function (err, res, body) {
+                expect(res.statusCode).toBe(200);
+                expect(body).toEqual(expected);
+                done();
+              });
+          });
+      }
+    );
+  });
+
+  it('should update a keyword when receiving a POST at /api/keywords/:id', function(done) {
+    var expected = {
+      "_items": [
+        {'id': 1, 'value': 'Onion', 'categoryID': 2}
+      ]
+    };
+    var body = {'value': 'Onion', 'categoryID': 2};
+
+    async.series(
+      [
+        function(callback) {
+          resetDatabase(dbSession, callback);
+        },
+        function(callback) {
+          dbSession.insert('category',
+            {'name': 'Vegetable'},
+            function(err){
+              callback(err);
+            });
+        },
+        function(callback) {
+          dbSession.insert('category',
+            {'name': 'Utility'},
+            function(err){
+              callback(err);
+            });
+        },
+        function(callback) {
+          dbSession.insert('keyword',
+            {'value': 'Aubergine', 'categoryID': 1},
+            function(err){
+              callback(err);
+            });
+        },
+      ],
+      function(err, results) {
+        request.post(
+          {
+            'url': 'http://127.0.0.1:4445/api/keywords/1',
+            'body': body,
+            'json': true
+          },
+          function (err, res, body) {
+            if (err) throw(err);
+            expect(res.statusCode).toBe(200);
+            request.get(
+              {
+                'url': 'http://127.0.0.1:4445/api/keywords/',
+                'json': true
+              },
+              function (err, res, body) {
+                expect(res.statusCode).toBe(200);
+                expect(body).toEqual(expected);
+                done();
+              });
+          });
+      }
+    );
+  });
+
+  it('should remove a keyword when receiving a DELETE at /api/keywords/:id', function(done) {
+    var expected = {
+      "_items": [
+        {'id': 1, 'value': 'Aubergine', 'categoryID': 1}
+      ]
+    };
+
+    async.series(
+      [
+        function(callback) {
+          resetDatabase(dbSession, callback);
+        },
+        function(callback) {
+          dbSession.insert('category',
+            {'name': 'Vegetable'},
+            function(err){
+              callback(err);
+            });
+        },
+        function(callback) {
+          dbSession.insert('keyword',
+            {'value': 'Aubergine', 'categoryID': 1},
+            function(err){
+              callback(err);
+            });
+        },
+        function(callback) {
+          dbSession.insert('keyword',
+            {'value': 'Onion', 'categoryID': 1},
+            function(err){
+              callback(err);
+            });
+        }
+      ],
+      function(err, results) {
+        request.del(
+          {
+            'url': 'http://127.0.0.1:4445/api/keywords/2/',
+            'json': true
+          },
+          function (err, res, body) {
+            if (err) throw(err);
+            expect(res.statusCode).toBe(200);
+            request.get(
+              {
+                'url': 'http://127.0.0.1:4445/api/keywords/',
+                'json': true
+              },
+              function (err, res, body) {
+                expect(res.statusCode).toBe(200);
+                expect(body).toEqual(expected);
+                done();
+              });
+          });
+      }
+    );
+  });
+
 });
--- src/backend/server.js
+++ src/backend/server.js
@@ -18,6 +18,24 @@ var Server = function(port){
               res.collection(rows).send();
             }
           });
+      },
+      POST: function (req,res){
+        req.onJson(function(err, newKeyword){
+          if (err) {
+            console.log(err);
+            res.status.internalServerError(err);
+          } else {
+            dbSession.fetchAll('INSERT INTO keyword (value, categoryID) VALUES(?, ?)',
+              [newKeyword.value, newKeyword.categoryID], function(err,result){
+                if(err) {
+                  console.log(err);
+                  res.status.internalServerError(err);
+                } else {
+                  res.object({'status': 'ok', 'id': result.insertId}).send();
+                }
+              });
+          }
+        });
       }
     }
   );
@@ -36,6 +54,42 @@ var Server = function(port){
           });
       }
     }
+  );
+
+  server.route('/api/keywords/:id',
+    {
+      POST: function (req,res){
+        var keywordId = req.uri.child();
+        req.onJson(function(err, keyword){
+          if (err) {
+            console.log(err);
+            res.status.internalServerError(err);
+          } else {
+            dbSession.query('UPDATE keyword SET value=?, categoryID=? WHERE keyword.id=?',
+              [keyword.value, keyword.categoryID, keywordId], function(err,results){
+                if(err) {
+                  console.log(err);
+                  res.status.internalServerError(err);
+                } else {
+                  res.object({'status': 'ok'}).send();
+                }
+              });
+            }
+          });
+      },
+      DELETE: function (req,res){
+        var keywordId = req.uri.child();
+        dbSession.query('DELETE FROM keyword WHERE keyword.id=?',
+          [keywordId], function(err,results){
+            if(err) {
+              console.log(err);
+              res.status.internalServerError(err);
+            } else {
+              res.object({'status': 'ok'}).send();
+            }
+          });
+        }
+      }
   );
 
   return server;
